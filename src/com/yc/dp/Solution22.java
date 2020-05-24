2eff4f31d691503eba0d61da68834ff5e3e2bdda
Ychen9527 <Ychen9527@qq.com>
2020-05-24 16:43:58 +0800

    everyday commit-2

--- src/com/yc/dp/Solution22.java
+++ src/com/yc/dp/Solution22.java
@@ -1,44 +1,66 @@
 package com.yc.dp;
 
-public class Solution22 {//只有两个键的键盘 ⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐
-
-    public static int minSteps(int n) {//暴力DP
-        int[] dp = new int[n + 1];
-        dp[1] = 0;
-        for (int i = 2; i <= n; i++) {
-            int min = Integer.MAX_VALUE;
-            for (int j = 1; j < i; j++) {
-                if (i % j == 0) {
-                    min = Math.min(min, dp[j] + i / j);
-                }
-            }
-            dp[i] = min;
-        }
-        return dp[n];
-    }
-
-    public static int minSteps2(int n) {//DP优化
-        int[] dp = new int[n + 1];
-        int len = (int)Math.sqrt(n);
-        for (int i = 2; i <= n; i++) {
-            dp[i] = i;//素数就等与自身
-            for (int j = 2; j <=len; j++) {//<=
-                if (i % j == 0) {
-                    //如12可以转换成2+6，6则可以继续向下转换,为什么转换方程是这个？
-                    // 因为任何非素数都可以被2或3整除，2和3都等于本身，
-                    // dp[j]+i-j ---->dp[j] + dp[i / j];如何12可以最终转换成2*2*3 ---->2+2+3
-                    dp[i] = dp[j] + dp[i / j];
-                    break;//因为12=2*6或3*4结果是 一样的，所以找到就立刻返回
-                }
+public class Solution22 {//乘积最大子数组 ⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐
+    public static int maxProduct(int[] nums) {
+        /*由于负数的原因，最大值乘以负数就变成了最小值，所有使用二维数组来解题
+        dp[i][j]：以 nums[i] 结尾的连续子数组的最值，计算最大值还是最小值由 j 来表示，j 就两个值；
+        当 j = 0 的时候，表示计算的是最小值；
+        当 j = 1 的时候，表示计算的是最大值
+        dp[i][0]：以 nums[i] 结尾的连续子数组的最小值
+        dp[i][1]：以 nums[i] 结尾的连续子数组的最大值*/
+        int len = nums.length;
+        if (len == 0) return nums[0];
+        int dp[][] = new int[len][2];
+        dp[0][0] = nums[0];
+        dp[0][1] = nums[0];
+        for (int i = 1; i < len; i++) {//从一开始，因为0已被赋初值
+            if (nums[i] > 0) {
+                dp[i][0] = Math.min(nums[i], dp[i - 1][0] * nums[i]);
+                dp[i][1] = Math.max(nums[i], dp[i - 1][1] * nums[i]);
+            } else {
+                dp[i][0] = Math.min(nums[i], dp[i - 1][1] * nums[i]);
+                dp[i][1] = Math.max(nums[i], dp[i - 1][0] * nums[i]);
             }
         }
-        return dp[n];
+
+        int max = dp[0][1];
+        for (int i = 0; i < len; i++) {
+            max = Math.max(dp[i][1], max);
+        }
+        return max;
     }
 
+    public static int maxProduct2(int[] nums) {//空间优化
+        int len = nums.length;
+        if (len == 0) return nums[0];
+
+        int preMax = nums[0];
+        int preMin = nums[0];
+
+        // 滚动变量
+        int curMax;
+        int curMin;
+
+        int result = nums[0];
+        for (int i = 1; i < len; i++) {
+            if (nums[i] >= 0) {
+                curMax = Math.max(preMax * nums[i], nums[i]);
+                curMin = Math.min(preMin * nums[i], nums[i]);
+            } else {
+                curMax = Math.max(preMin * nums[i], nums[i]);
+                curMin = Math.min(preMax * nums[i], nums[i]);
+            }
+            result = Math.max(result, curMax);
+
+            // 赋值滚动变量
+            preMax = curMax;
+            preMin = curMin;
+        }
+        return result;
+    }
 
     public static void main(String[] args) {
-        int i = minSteps2(2);
+        int i = maxProduct2(new int[]{-3, 0,-3});
         System.out.println(i);
     }
 }
-

7dd6e8c9f7de41f9b4b5d87150a05f572be50c6b
Ychen9527 <Ychen9527@qq.com>
2020-11-07 11:43:40 +0800

    everyday commit

--- src/com/yc/linkedlist/Solution2.java
+++ src/com/yc/linkedlist/Solution2.java
@@ -1,4 +1,48 @@
 package com.yc.linkedlist;
 
-public class Solution2 {
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Solution2 {//回文链表⭐⭐⭐⭐⭐⭐
+
+    public boolean isPalindrome(ListNode head) {//栈
+        //也可以使用ArrayList保存head.val,再用左右索引get之后进行比较
+        Deque<Integer> stack = new LinkedList<>();
+        ListNode p = head;//指针
+        while (head != null) {
+            stack.push(head.val);
+            head = head.next;
+
+        }
+        int len = stack.size() / 2;//减少比较次数,只比较一半的值
+        while (len-- > 0) {
+            if (p.val != stack.poll()) return false;
+            p = p.next;
+        }
+        return true;
+    }
+
+    /*--------------------------------------------*/
+    ListNode temp;
+
+    public boolean isPalindrome2(ListNode head) {//递归⭐⭐⭐⭐⭐⭐,画图理解
+        temp = head;
+        return check(head);
+    }
+
+    private boolean check(ListNode head) {
+        if (head == null) return true;
+        boolean res = check(head.next) && (temp.val == head.val);
+        temp = temp.next;
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Solution2 solution2 = new Solution2();
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(1);
+        boolean b = solution2.isPalindrome2(head);
+        System.out.println(b);
+    }
 }
---
+++ src/com/yc/linkedlist/Solution3.java
@@ -0,0 +1,20 @@
+package com.yc.linkedlist;
+
+public class Solution3 {//删除链表的节点⭐⭐⭐⭐⭐⭐
+
+    public ListNode deleteNode(ListNode head, int val) {
+        ListNode tmp = new ListNode(0);//哑节点
+        tmp.next = head;
+
+        ListNode pre = tmp;//指针,指向head(当前节点)的前一个节点
+        while (head != null) {
+            if (head.val == val) {
+                pre.next = head.next;
+            }
+            pre = head;
+            head = head.next;
+        }
+
+        return tmp.next;
+    }
+}
